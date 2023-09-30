b7b7ff64ee0946f7788a2577ff3b22103e154d85
oat431 <oat431@gmail.com>
2023-09-30 21:51:33 +0700

    🛠 setup: api gate way

---
+++ src/main/java/panomete/playground/tryapigateway/TryApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package panomete.playground.tryapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryApiGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryApiGateWayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/panomete/playground/tryapigateway/TryApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package panomete.playground.tryapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryApiGateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
